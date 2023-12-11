b0e87fd1f21a24e7cafe23dbfa8a04ee3ea7120b
Ektamadaan <ektamadaan45@gmail.com>
2023-12-11 21:33:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+//const value1 = prompt("Enter first value");
+//const value2 = prompt("Enter second value");
+ //alert("The final result is: " + finalresult(value1 , value2));
+
+function finalresult(value1 , value2) {
+	
+	return (value1 * 2) + value2 ;
+}
